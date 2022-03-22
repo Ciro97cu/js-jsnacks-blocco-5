c9fb16fac7238db1f8633744b7209e2b41b79c9d
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-22 15:21:48 +0100

    completed exercise

--- snack3/javascript/script.js
+++ snack3/javascript/script.js
@@ -1,1 +1,17 @@
 /* Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */
+
+function reverse(word) {
+    let reverseWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+
+        reverseWord += word[i];
+
+    }
+    return reverseWord;
+}
+
+let requestUser = prompt("Inserisci una parola");
+
+const wordToInvert = reverse(requestUser);
+
+console.log(wordToInvert);
