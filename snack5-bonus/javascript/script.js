2893f6266d8b7ed628014d66a899c3e0262c9b86
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-22 16:15:20 +0100

    completed snack

--- snack5-bonus/javascript/script.js
+++ snack5-bonus/javascript/script.js
@@ -1,3 +1,20 @@
 /* Scrivi una funzione che accetti tre argomenti: un array e due numeri 
 (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b” */
+
+function myFunction(array, num1, num2) {
+
+    const newArray = [];
+    for (let i = num1; i < num2 - 1; i++) {
+        newArray.push(array[i]);
+    }
+    return newArray;
+
+}
+
+const myArray = ["i", "t", "a", "l", "i", "a", "n"];
+const myNum = 2;
+const myNum2 = 7;
+
+const displayArray = myFunction(myArray, myNum, myNum2);
+console.log(displayArray);
