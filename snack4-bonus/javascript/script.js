c307ac2a866d2cf761bdc2a7b0c63329d49e5d98
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-22 15:43:35 +0100

    completed exercise

--- snack4-bonus/javascript/script.js
+++ snack4-bonus/javascript/script.js
@@ -1,3 +1,26 @@
 /* Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi)
 prendendo alternativamente gli elementi da uno e dall’altro,
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]. */
+
+const firstArray = ["a", "b", "c", "d", "e"];
+
+const secondArray = [1, 2, 3, 4, 5];
+
+const newArray = [];
+
+function joinArray(first, second) {
+
+
+    for (let i = 0; i < first.length; i++) {
+
+        newArray.push(first[i]);
+        newArray.push(second[i]);
+
+    }
+    return newArray;
+
+}
+
+const joinedArray = joinArray(firstArray, secondArray);
+
+console.log(joinedArray);
